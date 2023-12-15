af9358382666d6e87b171b31474391aeef759c15
Patrick <ptemy5@gmail.com>
2023-12-15 17:29:24 -0300

    rotas

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 import express from 'express'
+import rotas from './routes/rotas'
 const app = express()
 
 app.use(express.json())
+app.use(rotas)
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+  console.log('Server rodando na porta 3000')
 })
---
+++ src/routes/rotas.ts
@@ -0,0 +1,10 @@
+import { Router } from "express"
+const rota = Router()
+
+//listagem de carros
+//detalhes de um carro
+//cadastro de carro
+//atualização de um carro
+//exclusão do carro
+
+export default rota
