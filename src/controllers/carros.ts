450475dde456103515f5e4326829aae367e23b41
Patrick <ptemy5@gmail.com>
2023-12-18 18:21:44 -0300

    finalização

--- src/controllers/carros.ts
+++ src/controllers/carros.ts
@@ -4,7 +4,7 @@ import { Carro } from '../models/tipos'
 
 export const listarCarros = async (_: Request, res: Response) => {
   try {
-    const carros = await knex('carros')
+    const carros = await knex('carros').orderBy('id')
     res.status(200).json(carros)
   } catch {
     return res.status(500).json({ mensagem: 'Erro interno do servidor' })
@@ -29,8 +29,73 @@ export const detalharCarros = async (req: Request, res: Response) => {
   }
 }
 
-export const cadastrarCarros = async (req: Request, res: Response) => {}
+export const cadastrarCarros = async (req: Request, res: Response) => {
+  const { marca, modelo, cor, ano, valor } = req.body
 
-export const atualizarCarros = async (req: Request, res: Response) => {}
+  try {
+    const carro = await knex<Omit<Carro, 'id'>>('carros')
+      .insert({
+        marca,
+        modelo,
+        cor,
+        ano,
+        valor,
+      })
+      .returning('*')
 
-export const excluirCarros = async (req: Request, res: Response) => {}
+    res.status(201).json(carro)
+  } catch {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor' })
+  }
+}
+
+export const atualizarCarros = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const { marca, modelo, cor, ano, valor } = req.body
+
+  try {
+    const carro = await knex<Carro>('carros')
+      .where({ id: Number(id) })
+      .first()
+
+    if (!carro) {
+      return res.status(404).json({ mensagem: 'Carro não encontrado' })
+    }
+
+    await knex<Carro>('carros')
+      .update({
+        marca,
+        modelo,
+        cor,
+        ano,
+        valor,
+      })
+      .where({ id: Number(id) })
+
+    res.status(204).send()
+  } catch {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor' })
+  }
+}
+
+export const excluirCarros = async (req: Request, res: Response) => {
+  const { id } = req.params
+
+  try {
+    const carro = await knex<Carro>('carros')
+      .where({ id: Number(id) })
+      .first()
+
+    if (!carro) {
+      return res.status(404).json({ mensagem: 'Carro não encontrado' })
+    }
+
+    await knex<Carro>('carros')
+      .where({ id: Number(id) })
+      .del()
+
+    res.status(204).send()
+  } catch {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor' })
+  }
+}
